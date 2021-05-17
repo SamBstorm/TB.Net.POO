57f4e7ee643a32bffb0df34b1991baab225ea35f
Legrain Samuel <samuel.legrain@bstorm.be>
2021-05-17 14:32:03 +0200

    Add Demos StructuredClass

---
+++ TB.Net.POO.OrganisationClass/StructuredClass.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace TB.Net.POO.OrganisationClass
+{
+    public class StructuredClass
+    {
+        #region Champs/Variables Membres
+
+        #endregion
+
+        #region Propriétés
+        #region Propriétés dépendant d'un champs
+
+        #endregion
+        #region Propriétés Auto-Calculés
+
+        #endregion
+        #region Auto-propriété
+
+        #endregion
+        #region Indexeur
+
+        #endregion
+        #endregion
+
+        #region Constructeurs/Destructeur
+
+        #endregion
+
+        #region Méthodes
+        #region Méthode privé (utilisable par ma class)
+
+        #endregion
+        #region Méthode public
+
+        #endregion
+        #region Méthode override
+
+        #endregion
+        #endregion
+    }
+}
