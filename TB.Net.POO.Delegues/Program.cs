7f8db336be0da6a47c5a336ab629ba38ac7b64ed
Samuel Legrain <samuel.legrain@bstorm.be>
2023-05-24 12:43:19 +0200

    Update Delegate Demo

--- TB.Net.POO.Delegues/Program.cs
+++ TB.Net.POO.Delegues/Program.cs
@@ -9,21 +9,27 @@ namespace TB.Net.POO.Delegues
             #region Démo Micro-Onde
             //Microonde mo = new Microonde();
 
-            //switch (Console.ReadLine())
+            //Console.WriteLine("Veuillez introduire les commandes, et finissez par 'FIN' pour démarrer...");
+            //string option;
+            //do
             //{
-            //    case "1":
-            //        mo.Program += mo.Defrost;
-            //        break;
-            //    case "2":
-            //        mo.Program += mo.Grill;
-            //        break;
-            //    case "3":
-            //        mo.Program += mo.Reheat;
-            //        break;
-            //    default:
-            //        break;
-            //}
-            //mo.Program(); 
+            //    Console.WriteLine("1.Defrost\n2.Grill\n3.Reheat");
+            //    option = Console.ReadLine();
+            //    switch (option)
+            //                {
+            //                    case "1":
+            //                        mo.Program += mo.Defrost;
+            //                        break;
+            //                    case "2":
+            //                        mo.Program += mo.Grill;
+            //                        break;
+            //                    case "3":
+            //                        mo.Program += mo.Reheat;
+            //                        break;
+            //                }
+            //} while (option !="FIN");
+
+            //mo.Program();
             #endregion
             #region Lecteur CD
             //CompactDisc CD = new CompactDisc(0, 120, 240, 360, 480, 600, 720, 840, 960, 1080, 1200);
@@ -37,16 +43,16 @@ namespace TB.Net.POO.Delegues
             //player.AddTrack(player.SelectTrack(2));
             //player.AddTrack(player.SelectTrack(1));
 
-            //player.Play(); 
+            //player.Play();
             #endregion
 
             #region IDE avec plug-in
             VisualStudioCode vsc = new VisualStudioCode(
-                new VisualStudioCodeExtension { Name ="C# easy refactoring"},
-                new VisualStudioCodeExtension { Name ="C# compilator"},
-                new VisualStudioCodeExtension { Name ="JS debugger"},
-                new VisualStudioCodeExtension { Name ="Angular debugger"},
-                new VisualStudioCodeExtension { Name ="Launch in browser"}
+                new VisualStudioCodeExtension { Name = "C# easy refactoring" },
+                new VisualStudioCodeExtension { Name = "C# compilator" },
+                new VisualStudioCodeExtension { Name = "JS debugger" },
+                new VisualStudioCodeExtension { Name = "Angular debugger" },
+                new VisualStudioCodeExtension { Name = "Launch in browser" }
                 );
             #endregion
         }
